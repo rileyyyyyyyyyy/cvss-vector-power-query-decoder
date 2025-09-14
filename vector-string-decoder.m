a44b29f7b4789d144b010c05788f98b603dcf83e
rileyyyyyyyyyy <167968952+rileyyyyyyyyyy@users.noreply.github.com>
2025-09-15 03:42:07 +1000

    Adds mapping tables based on CVSS definitions

--- vector-string-decoder.m
+++ vector-string-decoder.m
@@ -28,5 +28,40 @@ let
         else if List.ContainsAny(metrics, {"PR","UI","S"}) then "3.x"
         else if List.Contains(metrics, "Au") then "2.0"
         else "unknown",
+
+    // mapping tables
+    map_v2 = #table({"Metric","Code","Name","Readable"}, {
+        {"AV","N","Access Vector","Network"}, {"AV","A","Access Vector","Adjacent Network"}, {"AV","L","Access Vector","Local"},
+        {"AC","H","Access Complexity","High"}, {"AC","M","Access Complexity","Medium"}, {"AC","L","Access Complexity","Low"},
+        {"Au","N","Authentication","None"}, {"Au","S","Authentication","Single"}, {"Au","M","Authentication","Multiple"},
+        {"C","N","Confidentiality","None"}, {"C","P","Confidentiality","Partial"}, {"C","C","Confidentiality","Complete"},
+        {"I","N","Integrity","None"}, {"I","P","Integrity","Partial"}, {"I","C","Integrity","Complete"},
+        {"A","N","Availability","None"}, {"A","P","Availability","Partial"}, {"A","C","Availability","Complete"}
+    }),
+    map_v3 = #table({"Metric","Code","Name","Readable"}, {
+        {"AV","N","Attack Vector","Network"}, {"AV","A","Attack Vector","Adjacent"}, {"AV","L","Attack Vector","Local"}, {"AV","P","Attack Vector","Physical"},
+        {"AC","L","Attack Complexity","Low"}, {"AC","H","Attack Complexity","High"},
+        {"PR","N","Privileges Required","None"}, {"PR","L","Privileges Required","Low"}, {"PR","H","Privileges Required","High"},
+        {"UI","N","User Interaction","None"}, {"UI","R","User Interaction","Required"},
+        {"S","U","Scope","Unchanged"}, {"S","C","Scope","Changed"},
+        {"C","H","Confidentiality","High"}, {"C","L","Confidentiality","Low"}, {"C","N","Confidentiality","None"},
+        {"I","H","Integrity","High"}, {"I","L","Integrity","Low"}, {"I","N","Integrity","None"},
+        {"A","H","Availability","High"}, {"A","L","Availability","Low"}, {"A","N","Availability","None"}
+    }),
+    map_v4 = #table({"Metric","Code","Name","Readable"}, {
+        {"AV","N","Attack Vector","Network"}, {"AV","A","Attack Vector","Adjacent"}, {"AV","L","Attack Vector","Local"}, {"AV","P","Attack Vector","Physical"},
+        {"AC","L","Attack Complexity","Low"}, {"AC","H","Attack Complexity","High"},
+        {"AT","N","Attack Requirements","None"}, {"AT","P","Attack Requirements","Present"},
+        {"PR","N","Privileges Required","None"}, {"PR","L","Privileges Required","Low"}, {"PR","H","Privileges Required","High"},
+        {"UI","N","User Interaction","None"}, {"UI","P","User Interaction","Passive"}, {"UI","A","User Interaction","Active"},
+        {"VC","H","Vulnerable Confidentiality","High"}, {"VC","L","Vulnerable Confidentiality","Low"}, {"VC","N","Vulnerable Confidentiality","None"},
+        {"VI","H","Vulnerable Integrity","High"}, {"VI","L","Vulnerable Integrity","Low"}, {"VI","N","Vulnerable Integrity","None"},
+        {"VA","H","Vulnerable Availability","High"}, {"VA","L","Vulnerable Availability","Low"}, {"VA","N","Vulnerable Availability","None"},
+        {"SC","H","Subsequent Confidentiality","High"}, {"SC","L","Subsequent Confidentiality","Low"}, {"SC","N","Subsequent Confidentiality","None"},
+        {"SI","H","Subsequent Integrity","High"}, {"SI","L","Subsequent Integrity","Low"}, {"SI","N","Subsequent Integrity","None"},
+        {"SA","H","Subsequent Availability","High"}, {"SA","L","Subsequent Availability","Low"}, {"SA","N","Subsequent Availability","None"}
+    }),
+    mapAll = Table.Combine({map_v2, map_v3, map_v4}), // combine mapping tables into one table
+
 in
-    detected
+    mapAll
