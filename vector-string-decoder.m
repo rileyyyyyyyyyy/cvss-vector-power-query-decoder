63f08973e9332eaa2c14eec3358e473f10a77da5
rileyyyyyyyyyy <167968952+rileyyyyyyyyyy@users.noreply.github.com>
2025-09-15 03:40:02 +1000

    Adds version detection from context

--- vector-string-decoder.m
+++ vector-string-decoder.m
@@ -19,5 +19,14 @@ let
     // create table for key-value pairs
     kvTable = if List.Count(parts)=0 then #table({"Metric","Code"}, {})
               else Table.FromColumns({ List.Transform(parts, each _{0}), List.Transform(parts, each _{1}) }, {"Metric","Code"}),
+
+    // detect version from context
+    metrics = if Table.IsEmpty(kvTable) then {} else List.Distinct(kvTable[Metric]),
+    detected =
+        if versionHint <> null then versionHint
+        else if List.ContainsAny(metrics, {"AT","VC","VI","VA","SC","SI","SA"}) then "4.0"
+        else if List.ContainsAny(metrics, {"PR","UI","S"}) then "3.x"
+        else if List.Contains(metrics, "Au") then "2.0"
+        else "unknown",
 in
-    kvTable
+    detected
